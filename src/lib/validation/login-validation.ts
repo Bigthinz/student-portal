2037a65bf5c5e8a48853ba8d1e5a8e3f541c2aa7
David Dzineku <44753593+Bigthinz@users.noreply.github.com>
2023-06-27 14:43:06 +0000

    fix: validation

--- src/lib/validation/login-validation.ts
+++ src/lib/validation/login-validation.ts
@@ -3,12 +3,12 @@ import { z } from 'zod';
 export type LoginSchema = z.infer<typeof loginSchema>;
 
 
-const idRegex = /^ueb.*/;
 
 export const loginSchema = z.object({
   studentId: z
     .string()
-    .regex(idRegex, 'Please enter a valid student ID'),
+    .min(5, { message: 'Please enter a valid student Id ' }),
+
   password: z
     .string()
     .min(8, { message: 'Password must be at least 8 characters long' }),
